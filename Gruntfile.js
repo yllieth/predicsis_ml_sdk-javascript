cba32737107c53a754b805dd5cd3110b0c43a8dd
yllieth <sylvnimes@hotmail.com>
2015-03-09 11:34:25 +0100

    add grunt tasks
    - buildcontrol (update documentation on gh-pages)
    - clean (remove previous builds)
    - concat (put all models together)
    - ngdocs (generate documentation)
    - uglify (minify js)
    
    - build = clean + concat + uglify
    - doc[:deploy] = ngdocs

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    buildcontrol: {
+      options: {
+        dir: 'doc',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      github: {
+        options: {
+          remote: 'git@github.com:yllieth/predicsis_ml_sdk-javascript.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
+
+    // remove previous builds
+    clean: {
+      dist: ['dist']
+    },
+
+    // build all model files into a single js file
+    concat: {
+      dist: {
+        src: ['lib/model/*.js'],
+        dest: 'dist/predicsisMLSDK.js'
+      }
+    },
+
+    // generate documentation of MLStudio source code
+    ngdocs: {
+      options: {
+        dest: 'docs/',
+        title: 'Documentation',
+        //scripts: ['vendor/angular/angular.js'],
+        styles: ['docs/ngdoc.css'],
+        html5Mode: false,
+        startPage: '/api',
+        titleLink: '/api',
+        bestMatch: true
+      },
+      api: {
+        src: ['lib/model/*.js', 'lib/helper/*.js']
+      }
+    },
+
+    // remove comments
+    uglify: {
+      options: {
+        mangle: false,
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/predicsisMLSDK.min.js': ['dist/predicsisMLSDK.js']
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('build', ['clean', 'concat', 'uglify']);
+
+  grunt.registerTask('doc', function(target) {
+    grunt.task.run(['ngdocs']);
+
+    if (target === 'deploy') {
+      grunt.task.run(['buildcontrol:github']);
+    }
+  });
+
+};
